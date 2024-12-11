6f34e892cba52935d105948719fc12b2fdde27d0
iFruze <denisbyev4s@gmail.com>
2024-12-11 12:33:59 +0300

    change auth_service

--- auth/auth_service.py
+++ auth/auth_service.py
@@ -1,51 +1,29 @@
-# auth_service.py
-from fastapi import FastAPI, Depends, HTTPException
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from fastapi_jwt_auth import AuthJWT
-from fastapi_jwt_auth.exceptions import AuthJWTException
+from fastapi import FastAPI, HTTPException, Depends, Response
+from authx import AuthX, AuthXConfig
 from pydantic import BaseModel
-from starlette.responses import JSONResponse
 
 app = FastAPI()
+config = AuthXConfig()
+config.JWT_SECRET_KEY = "SECRET_KEY"
+config.JWT_ACCESS_COOKIE_NAME = "my_access_token"
+config.JWT_TOKEN_LOCATION = ["cookies"]
 
-class Settings(BaseModel):
-    authjwt_secret_key: str = "your_secret_key"
+security = AuthX(config=config)
 
-@AuthJWT.load_config
-def get_config():
-    return Settings()
-
-@app.exception_handler(AuthJWTException)
-def authjwt_exception_handler(request, exc):
-    return JSONResponse(
-        status_code=exc.status_code,
-        content={"detail": exc.message}
-    )
-
-class User(BaseModel):
+class UserLoginSchema(BaseModel):
     username: str
     password: str
 
-fake_db = {
-    "test": {
-        "username": "test",
-        "password": "test"
-    }
-}
+@app.post("/login")
+def login(creds: UserLoginSchema, response: Response):
 
-@app.post('/login')
-def login(user: User, Authorize: AuthJWT = Depends()):
-    if user.username not in fake_db or fake_db[user.username]["password"] != user.password:
-        raise HTTPException(status_code=401, detail="Bad username or password")
-    access_token = Authorize.create_access_token(subject=user.username)
-    return {"access_token": access_token}
+    if(creds.username == "Admin1" and creds.password == "df23f"):
+        token = security.create_access_token(uid="1")
+        response.set_cookie(config.JWT_ACCESS_COOKIE_NAME, token)
+        return {"access_token": token}
+    raise HTTPException(status_code=401, detail="Неверный логин или пароль.")
 
-@app.get('/verify-token')
-def verify_token(Authorize: AuthJWT = Depends()):
-    Authorize.jwt_required()
+@app.get("/protected", dependencies=[Depends(security.access_token_required)])
+def protected(Authorize: AuthX = Depends()):
     current_user = Authorize.get_jwt_subject()
     return {"user": current_user}
-
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=8001)
