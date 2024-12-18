ba9029294bdc6b1481d94dbb7682dcc9ad5448df
Ilya Sachok <isachek2001@gmail.com>
2024-12-18 17:22:06 +0300

    Add table example for frontend part

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,89 @@
+<!--<template>-->
+<!--  <div class="hello">-->
+<!--    <h1>{{ msg }}</h1>-->
+<!--    <p>-->
+<!--      For a guide and recipes on how to configure / customize this project,<br>-->
+<!--      check out the-->
+<!--      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.-->
+<!--    </p>-->
+<!--    <h3>Installed CLI Plugins</h3>-->
+<!--    <ul>-->
+<!--      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>-->
+<!--      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>-->
+<!--    </ul>-->
+<!--    <h3>Essential Links</h3>-->
+<!--    <ul>-->
+<!--      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>-->
+<!--      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>-->
+<!--      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>-->
+<!--      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>-->
+<!--      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>-->
+<!--    </ul>-->
+<!--    <h3>Ecosystem</h3>-->
+<!--    <ul>-->
+<!--      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>-->
+<!--      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>-->
+<!--      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>-->
+<!--      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>-->
+<!--      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>-->
+<!--    </ul>-->
+<!--  </div>-->
+<!--</template>-->
+
+<!--<script lang="ts">-->
+<!--import { defineComponent } from 'vue';-->
+
+<!--export default defineComponent({-->
+<!--  name: 'HelloWorld',-->
+<!--  props: {-->
+<!--    msg: String,-->
+<!--  },-->
+<!--});-->
+<!--</script>-->
+
+<!--&lt;!&ndash; Add "scoped" attribute to limit CSS to this component only &ndash;&gt;-->
+<!--<style scoped>-->
+<!--h3 {-->
+<!--  margin: 40px 0 0;-->
+<!--}-->
+<!--ul {-->
+<!--  list-style-type: none;-->
+<!--  padding: 0;-->
+<!--}-->
+<!--li {-->
+<!--  display: inline-block;-->
+<!--  margin: 0 10px;-->
+<!--}-->
+<!--a {-->
+<!--  color: #42b983;-->
+<!--}-->
+<!--</style>-->
+
+<template>
+  <EasyDataTable
+    :headers="headers"
+    :items="items"
+  />
+</template>
+
+<script lang="ts" setup>
+import type { Header, Item } from "vue3-easy-data-table";
+
+const headers: Header[] = [
+  { text: "PLAYER", value: "player" },
+  { text: "TEAM", value: "team"},
+  { text: "NUMBER", value: "number"},
+  { text: "POSITION", value: "position"},
+  { text: "HEIGHT", value: "indicator.height"},
+  { text: "WEIGHT (lbs)", value: "indicator.weight", sortable: true},
+  { text: "LAST ATTENDED", value: "lastAttended", width: 200},
+  { text: "COUNTRY", value: "country"},
+];
+
+const items: Item[] = [
+  { player: "Stephen Curry", team: "GSW", number: 30, position: 'G', indicator: {"height": '6-2', "weight": 185}, lastAttended: "Davidson", country: "USA"},
+  { player: "Lebron James", team: "LAL", number: 6, position: 'F', indicator: {"height": '6-9', "weight": 250}, lastAttended: "St. Vincent-St. Mary HS (OH)", country: "USA"},
+  { player: "Kevin Durant", team: "BKN", number: 7, position: 'F', indicator: {"height": '6-10', "weight": 240}, lastAttended: "Texas-Austin", country: "USA"},
+  { player: "Giannis Antetokounmpo", team: "MIL", number: 34, position: 'F', indicator: {"height": '6-11', "weight": 242}, lastAttended: "Filathlitikos", country: "Greece"},
+];
+</script>
